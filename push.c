6feca1de33e82219ae289226f01ea440be649340
DudeGFA <favouridowuagida0@gmail.com>
2022-07-14 21:05:12 +0100

    Update push.c

--- push.c
+++ push.c
@@ -26,7 +26,7 @@ void add_dnodeint(stack_t **head, unsigned int line_no)
 	{
         no_int_arg_err(line_no);
 	}
-	printf("before atoi")
+	printf("before atoi");
     _arg = atoi(&(stripped_line[4]));
 	printf("%d", _arg);
 	new_head_node = malloc(sizeof(stack_t));
