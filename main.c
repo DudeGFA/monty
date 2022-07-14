a5a8485fce6c515895936cdfb7abed7bf6997ebd
DudeGFA <favouridowuagida0@gmail.com>
2022-07-14 21:25:57 +0100

    more debugging

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int _arg;
 char *stripped_line;
 void not_file(char *file_name)
 {
--- push.c
+++ push.c
@@ -12,11 +12,12 @@ void no_int_arg_err(unsigned int line_no)
     exit(EXIT_FAILURE);
 }
 
-void add_dnodeint(stack_t **head, unsigned int line_no __attribute__((unused)))
+void add_dnodeint(stack_t **head, unsigned int line_no)
 {
 	stack_t *new_head_node;
+	int new_arg;
 
-	/*printf("%s first check\n", stripped_line);
+	printf("%s first check\n", stripped_line);
     if (_strlen(stripped_line) < 5)
 	{
         no_int_arg_err(line_no);
@@ -24,18 +25,19 @@ void add_dnodeint(stack_t **head, unsigned int line_no __attribute__((unused)))
 	printf("%c second check\n", stripped_line[4]);
     if (!isdigit(stripped_line[4]))
 	{
+		printf("in isdigit")
         no_int_arg_err(line_no);
 	}
-	printf("before atoi\n");*/
-    _arg = atoi(&(stripped_line[4]));
-	printf("%d", _arg);
+	printf("before atoi\n");
+    new_arg = atoi(&(stripped_line[4]));
+	printf("%d", new_arg);
 	new_head_node = malloc(sizeof(stack_t));
 	if (new_head_node == NULL)
     {
 		fprintf(stderr, "Error: malloc failed");
         exit (EXIT_FAILURE);
     }
-	new_head_node->n = _arg;
+	new_head_node->n = new_arg;
 	new_head_node->prev = NULL;
 	new_head_node->next = *head;
 	if ((*head) != NULL)
