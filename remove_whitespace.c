075de40381c07af4652e1ddb5de5e865ee2d1f66
DudeGFA <favouridowuagida0@gmail.com>
2022-07-15 01:05:51 +0100

    Update remove_whitespace.c

--- remove_whitespace.c
+++ remove_whitespace.c
@@ -11,7 +11,7 @@ int _strlen(char *stri)
 char *remove_whitespace(char *old_str)  
 {
     unsigned int i, j = 0;
-    char new_str[_strlen(old_str)];
+    char new_str[30];
     char *p = new_str;
 
     for(i = 0; old_str[i]; i++)
