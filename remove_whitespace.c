00bb1fc592022ef6a48db46ef135ed25a80664e8
DudeGFA <favouridowuagida0@gmail.com>
2022-07-15 01:19:47 +0100

    Update remove_whitespace.c

--- remove_whitespace.c
+++ remove_whitespace.c
@@ -13,7 +13,7 @@ char *remove_whitespace(char *old_str)
     unsigned int i, j = 0;
     char new_str[7];
     char *p;
-
+    printf("oldstr: %s\n", old_str);
     for(i = 0; old_str[i]; i++)
     {
         if (old_str[i] != ' ' && old_str[i] != '\t' && old_str[i] != '\n')
